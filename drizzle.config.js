411256e14082f82bd14e56410d4691de31b9f27c
Ankith M <ankithankith022@gmail.com>
2025-04-06 18:43:12 +0530

    #changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:iwoIU0zEZT6f@ep-steep-sunset-a5b8pkqj.us-east-2.aws.neon.tech/AI-Content-Generator?sslmode=require',
+      url: 'postgresql://neondb_owner:iwoIU0zEZT6f@ep-steep-sunset-a5b8pkqj-pooler.us-east-2.aws.neon.tech/AI-Content-Generator?sslmode=require',
     }
   };
   
